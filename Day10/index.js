b957140426da083939cc69200451086e0213ba06
reem <rowanalaa176@gmail.com>
2026-09-04 10:39:04 +0300

    add Day10 tasks

---
+++ Day10/index.js
@@ -0,0 +1,15 @@
+export function add(a,b) {
+    return a+b;
+}
+export function multi(a,b) {
+    return a*b;
+}
+export function subtract(a,b) {
+    return a-b;
+}
+export function divide(a,b) {
+    return a/b;
+}
+export function modulo(a,b) {
+    return a%b;
+}
---
+++ Day10/test.js
@@ -0,0 +1,6 @@
+import * as mathoperations from './index.js';
+console.log(mathoperations.add(5,5));
+console.log(mathoperations.subtract(5,5));
+console.log(mathoperations.divide(5,5));
+console.log(mathoperations.modulo(5,5));
+console.log(mathoperations.multi(5,5));
